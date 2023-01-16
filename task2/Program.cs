afa833288407fd1ac913287782e1078c5e73b716
Elena Karpovich <elekarpovi@tut.by>
2023-01-16 23:05:06 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+System.Console.WriteLine("Нужно ввести три числа.");
+System.Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max=a;
+
+if (b>max) max=b;
+if (c>max) max=c;
+
+System.Console.WriteLine($"Максимальное число {max}");
+
