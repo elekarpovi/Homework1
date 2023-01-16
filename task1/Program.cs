ca6edbc947d98c4b6b38763cfda2be49abc248eb
Elena Karpovich <elekarpovi@tut.by>
2023-01-16 22:16:48 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+System.Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    System.Console.WriteLine($"{a} большее, {b} меньшее");
+}
+else
+{
+    System.Console.WriteLine($"{b} большее, {a} меньшее");
+}
