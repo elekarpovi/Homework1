e7f642ecd0a549eccfae1160bb157d421a7caaff
Elena Karpovich <elekarpovi@tut.by>
2023-01-17 00:11:49 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N>0)
+{
+    int i = 2;
+while (i <= N)
+{
+    System.Console.WriteLine($"{i}");
+    i=i+2;
+}
+}
+else 
+{
+    int i = 0;
+    while (i >= N)
+    {
+       System.Console.WriteLine($"{i}");
+    i=i-2; 
+    }
+}
