f99477ffc1494a14e5edc8ce11391ceac17766ec
Elena Karpovich <elekarpovi@tut.by>
2023-01-16 23:17:56 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+System.Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    System.Console.WriteLine($"{a} является четным числом");
+}
+
+else
+{
+    System.Console.WriteLine($"{a} не является четным числом");
+}
